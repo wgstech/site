2032c4fa318f74b4128f643a9da35f755aa099ae
Leonardo Gurgel <leo@gurgel.io>
2025-03-11 11:47:34 -0300

    fix header sticky position

--- src/lib/layout/navbar/header.tsx
+++ src/lib/layout/navbar/header.tsx
@@ -4,13 +4,11 @@ import { LogoWithText } from "./logo";
 
 export function Header() {
   return (
-    <header className="w-full bg-slate-100/90 sticky z-100 backdrop-blur-sm text-slate-900">
+    <header className="w-full bg-slate-100/90 sticky top-0 z-100 backdrop-blur-sm text-slate-900">
       <nav className="px-4 mx-auto container flex items-center justify-between transition-colors duration-500">
         <LogoWithText />
         <div className="justify-end gap-8 w-full flex h-full items-center">
-          <NavLink href="/">Soluções</NavLink>
           <NavLink href="https://solicita.wgstech.com.br">Suporte</NavLink>
-          <NavLink href="/">Sobre</NavLink>
         </div>
       </nav>
     </header>
