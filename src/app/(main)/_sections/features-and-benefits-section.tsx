4b170fe254b480ff5074ef91340c85f38bb59f6e
Leonardo Gurgel <leo@gurgel.io>
2025-03-25 22:34:31 -0300

    update features-and-benefits-section.tsx

--- src/app/(main)/_sections/features-and-benefits-section.tsx
+++ src/app/(main)/_sections/features-and-benefits-section.tsx
@@ -5,10 +5,9 @@ import { SectionContainer } from "@/lib/components/ui/section-container";
 import { SectionTitle } from "@/lib/components/ui/section-title";
 import {
   ArrowRight,
-  Cloud,
+  CurrencyDollarSimple,
   Gear,
-  LockKey,
-  TrendUp,
+  ShieldCheck,
 } from "@phosphor-icons/react/dist/ssr";
 import { BenefitCard } from "./components/benefit-card";
 
@@ -18,48 +17,37 @@ export function FeaturesAndBenefits() {
       <SectionTitle>
         Recursos e Benefícios que Impulsionam Seu Sucesso
       </SectionTitle>
-      <div className="mt-14 grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-12">
+      <div className="mt-14 grid grid-cols-1 gap-5 md:grid-cols-3 md:gap-12">
         <BenefitCard
           title="Soluções Personalizadas"
           Icon={Gear}
           className="motion-delay-300"
         >
-          Entendemos que cada empresa é única. Por isso, oferecemos soluções de
-          TI personalizadas para atender às suas necessidades e objetivos
-          específicos.
+          Soluções de TI personalizadas para atender às suas necessidades e
+          objetivos específicos.
         </BenefitCard>
         <BenefitCard
-          title="Otimização de Infraestrutura"
-          Icon={TrendUp}
+          title="Redução de Custos"
+          Icon={CurrencyDollarSimple}
           className="motion-delay-400"
         >
-          Analisamos sua infraestrutura de TI e implementamos as melhores
-          práticas para otimizar o desempenho, reduzir custos e aumentar a
-          segurança.
+          Consultoria para identificar e eliminar desperdícios na sua
+          infraestrutura.
         </BenefitCard>
         <BenefitCard
-          title="Segurança da Informação"
-          Icon={LockKey}
+          title="Proteção Máxima"
+          Icon={ShieldCheck}
           className="motion-delay-500"
         >
-          Implementamos soluções de segurança robustas para proteger seus dados
-          e sua empresa contra vírus, hackers e outras ameaças cibernéticas.
-        </BenefitCard>
-        <BenefitCard
-          title="Computação em Nuvem"
-          Icon={Cloud}
-          className="motion-delay-600"
-        >
-          A computação em nuvem oferece flexibilidade, escalabilidade e economia
-          para o seu negócio. Ajudamos você a migrar para a nuvem de forma
-          segura e eficiente.
+          Soluções de segurança robustas para proteger seus dados e sua empresa
+          contra ameaças.
         </BenefitCard>
       </div>
       <MotionInView className="motion-delay-700 motion-safe:motion-preset-fade-lg motion-safe:motion-slide-up-lg">
         <ContactFormModal>
           <Button size="lg" className="mx-auto mt-14" type="button">
-            <ArrowRight />
             Estou interessado
+            <ArrowRight weight="bold" />
           </Button>
         </ContactFormModal>
       </MotionInView>
